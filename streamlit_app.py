eafaa6e81ec29641a6db32f8df256e9365d4b565
Vladimir S. <slow9v+git@gmail.com>
2025-04-25 14:11:35 +0300

    feat: tf compute for the single text file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,24 @@ import streamlit as st
 
 import tfidf
 from logger import info
+from tfidf import compute_tf
 
 info("Streamlit App code touched...")
-st.title("Helloworld - The Streamlit App")
-uploaded_file = st.file_uploader("Upload text file", type=["txt"])
+st.title("Lesta TD-IFD Analyser - The Streamlit App")
+uploaded_file = st.file_uploader("Upload the text file to analyze below: ", type=["txt"])
 if uploaded_file is not None:
     file_contents = uploaded_file.read().decode("utf-8")
 
     # st.subheader("File content")
     # st.text(file_contents)
-    processed_text = " ".join(tfidf.tokenize(file_contents))
+    # processed_text = " ".join(tfidf.tokenize(file_contents))
+    tokens = tfidf.tokenize(file_contents)
 
-    st.subheader("Bare russian tokens got from text file")
-    st.text(processed_text)
+    df_tf = compute_tf(tokens, 50)
+
+    st.subheader("Terms Frequency for the given text (Russian only)")
+    df_tf.index = df_tf.index + 1 # make index 1-based for display
+    st.dataframe(df_tf)
+
+    # st.subheader("Bare russian tokens got from text file")
+    # st.text(processed_text)
--- tfidf.py
+++ tfidf.py
@@ -1,4 +1,7 @@
 import re
+from collections import Counter
+
+import pandas as pd
 from pymystem3 import Mystem
 
 from config import STOP_WORDS_PATH
@@ -10,6 +13,7 @@ with open(STOP_WORDS_PATH, encoding="utf-8") as f:
 mystem = Mystem()
 re_russian_word = re.compile(r'\b[а-яА-ЯёЁ]+\b')
 
+
 def tokenize(text: str) -> list:
     tokens_iterator = re_russian_word.finditer(text)
     tokens = [match.group() for match in tokens_iterator
@@ -19,3 +23,20 @@ def tokenize(text: str) -> list:
     result_tokens = [t.strip() for t in lemmatized_tokens if t.strip().isalpha()]
     return result_tokens
 
+
+def compute_tf(tokens: list[str], top_n: int = 50) -> pd.DataFrame:
+    """
+    Compute TF (Term Frequency) for a single document represented as tokens list.
+    Returns a DataFrame with top_n rows sorted by descending TF.
+
+    """
+    assert tokens, "tokens list should not be empty"
+
+    terms_counts = Counter(tokens) # dict {'term1': <term1_count>, ...}
+    total_words = sum(terms_counts.values())
+
+    # Convert the count dict to DataFrame
+    df = pd.DataFrame([(word, count / total_words) for word, count in terms_counts.items()],
+                      columns=['word', 'tf'])
+    df_sorted = df.sort_values(by="tf", ascending=False).head(top_n).reset_index(drop=True)
+    return df_sorted
