d4cda94069878c5cdaa729e81a49614f156d0b28
Vladimir S. <slow9v+GIT@gmail.com>
2025-04-26 15:13:44 +0300

    refactor: use nlp-context for the single document, prepare for handling in SQLite

---
+++ nlp.py
@@ -0,0 +1,80 @@
+import lzma
+import re
+from collections import Counter
+from dataclasses import dataclass
+from typing import Optional
+
+import xxhash
+from pymystem3 import Mystem
+
+from config import STOP_WORDS_PATH
+
+
+@dataclass
+class NlpDocContext:
+    text_input: str
+    xxhash64_text: Optional[int] = None
+    compressed_text: Optional[bytes] = None
+    tokens_lemmatized: Optional[list] = None
+    token_count_map: Optional[dict[str, int]] = None
+    token_tf_map: Optional[dict[str, float]] = None
+
+    def clear(self) -> None:
+        """
+        Forcefully clears all fields, setting them to None,
+        even if it contradicts the expected data type of field,
+        for the sake of memory efficiency.
+        """
+        for field_name in self.__annotations__:
+            object.__setattr__(self, field_name, None)
+
+
+# Russian stop-words preloaded from the SpaCy Large model
+with open(STOP_WORDS_PATH, encoding="utf-8") as f:
+    stop_words_set = set(line.strip() for line in f if line.strip())
+
+mystem = Mystem()
+re_russian_word = re.compile(r'\b[а-яА-ЯёЁ]+\b')
+
+
+def tokenize(nlp: NlpDocContext, text: str = "") -> Optional[list]:
+    if not nlp.text_input and not text:
+        raise ValueError("Text must be presented in 'nlp.text_input' or 'text' argument.")
+    if not text:
+        text = nlp.text_input
+    elif not nlp.text_input:
+        nlp.text_input = text
+    else:
+        raise ValueError("The 'text' argument given, but 'nlp.text_input' is not empty.")
+
+    tokens_iterator = re_russian_word.finditer(text)
+    tokens = [match.group() for match in tokens_iterator
+              if match.group().lower() not in stop_words_set]
+    lemmas = mystem.lemmatize(" ".join(tokens))
+    # filter lemmatized tokens to ensure 'words contained letters only'
+    tokens_lemmatized = [t.strip() for t in lemmas if t.strip().isalpha()]
+
+    nlp.tokens_lemmatized = tokens_lemmatized
+    return tokens_lemmatized if tokens_lemmatized else None
+
+
+def compute_tf(nlp: NlpDocContext) -> Optional[dict[str, float]]:
+    """ Compute TF (Term Frequency) for a single document represented as tokens list. """
+
+    assert nlp.tokens_lemmatized, "'nlp.tokens_lemmatized' list should not be empty"
+
+    nlp.token_count_map = Counter(nlp.tokens_lemmatized)  # dict {'token1': <token1_count>, ...}
+    total_words = sum(nlp.token_count_map.values())
+
+    nlp.token_tf_map = {word: count / total_words for word, count in nlp.token_count_map.items()}
+    return nlp.token_tf_map if nlp.token_tf_map else None
+
+
+def hash_original_text(nlp: NlpDocContext) -> Optional[int]:
+    nlp.xxhash64_text = xxhash.xxh64(nlp.text_input.encode('utf-8')).intdigest()
+    return nlp.xxhash64_text if nlp.xxhash64_text else None
+
+
+def compress_original_text(nlp: NlpDocContext) -> Optional[bytes]:
+    nlp.compressed_text = lzma.compress(nlp.text_input.encode())
+    return nlp.compressed_text if nlp.compressed_text else None
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,14 @@ Run as `streamlit run streamlit_app.py`
 Running at `http://localhost:8501`
 
 """
+import humanize
+import pandas as pd
 import streamlit as st
 
-import tfidf
 from logger import info
-from tfidf import compute_tf
+from nlp import NlpDocContext, hash_original_text, tokenize, compute_tf, compress_original_text
+
+CORPUS_HASHMAP = {}  # for the quck duplicates detection   {xxh64: doc_id}
 
 info("Streamlit App code touched...")
 st.title("Lesta TD-IFD Analyser - The Streamlit App")
@@ -17,16 +20,25 @@ uploaded_file = st.file_uploader("Upload the text file to analyze below: ", type
 if uploaded_file is not None:
     file_contents = uploaded_file.read().decode("utf-8")
 
-    # st.subheader("File content")
-    # st.text(file_contents)
-    # processed_text = " ".join(tfidf.tokenize(file_contents))
-    tokens = tfidf.tokenize(file_contents)
+    nlp = NlpDocContext(file_contents)
+    try:
+        CORPUS_HASHMAP.update({hash_original_text(nlp): 'doc1'})
+        assert tokenize(nlp)
+        dict_tf = compute_tf(nlp)
 
-    df_tf = compute_tf(tokens, 50)
+        # Convert the count dict to DataFrame
+        df = pd.DataFrame(dict_tf.items(), columns=['word', 'tf'])
+        df_sorted = df.sort_values(by="tf", ascending=False).head(50).reset_index(drop=True)
 
-    st.subheader("Terms Frequency for the given text (Russian only)")
-    df_tf.index = df_tf.index + 1 # make index 1-based for display
-    st.dataframe(df_tf)
+        df_sorted.index = df_sorted.index + 1  # make index 1-based for display
+        st.subheader("Terms Frequency for the given text (Russian only)")
+        st.dataframe(df_sorted)
 
-    # st.subheader("Bare russian tokens got from text file")
-    # st.text(processed_text)
+        compressed_size = len(compress_original_text(nlp))
+        st.subheader("Info:")
+        st.info(f"Размер оригинального файла: {humanize.naturalsize(uploaded_file.size)}\n\n"
+                f"Хранение текста в базе данных будет стоить: {humanize.naturalsize(compressed_size)}")
+    except:
+        raise RuntimeError(f"Error during NLP-workflow on file '{uploaded_file.name}'")
+    finally:
+        nlp.clear()
